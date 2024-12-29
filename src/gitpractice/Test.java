b11c0bcf31cfd77014019a697b846220578f2a86
Thirumalrao Pathi <thirumal.pathi@gmail.com>
2024-12-29 13:09:50 +0400

    modifiying one file

--- src/gitpractice/Test.java
+++ src/gitpractice/Test.java
@@ -5,8 +5,11 @@ public class Test {
 	
 	public void printing() {
 	int a =5;
+	int c =7;
 	
 	System.out.println("printing number"+a);
+	System.out.println("printing one more line "+c);
+	
 	}
 
 }
