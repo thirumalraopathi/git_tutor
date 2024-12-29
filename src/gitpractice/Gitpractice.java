cb5d177829dedfd07d03032ffd8a40078acd6c5f
Thirumalrao Pathi <thirumal.pathi@gmail.com>
2024-12-29 13:11:10 +0400

    modified gitpractive file

--- src/gitpractice/Gitpractice.java
+++ src/gitpractice/Gitpractice.java
@@ -1,12 +1,9 @@
 package gitpractice;
 
 public class Gitpractice {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		System.out.println("welcome to hellow world");
-
+	
+	public void mian() {
+		System.out.println("updatiion");
 	}
 
 	
