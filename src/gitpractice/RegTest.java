8fcc174d98947a8930f8d678b695beead516af6b
Thirumalrao Pathi <thirumal.pathi@gmail.com>
2024-12-29 13:08:10 +0400

    adding additional files

---
+++ src/gitpractice/RegTest.java
@@ -0,0 +1,12 @@
+package gitpractice;
+
+public class RegTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("registration test");
+
+	}
+
+}
---
+++ src/gitpractice/Test.java
@@ -0,0 +1,12 @@
+package gitpractice;
+
+public class Test {
+	
+	
+	public void printing() {
+	int a =5;
+	
+	System.out.println("printing number"+a);
+	}
+
+}
